eb5b2ee41c4c4c08322dea6ce9b8427998a89ef0
Donghao Hong <hongdonghao0502@gmail.com>
2025-11-08 17:50:22 -0500

    Locate the face in the image

--- test_camera.py
+++ test_camera.py
@@ -4,14 +4,34 @@ import numpy as np
 # create an object for the camera
 cap = cv2.VideoCapture(0)
 
+# Haar cascade model (a type of machine learning based detector)
+# cv2.data.haarcascades gives the path to the OpenCV's built-in pretrained XML models
+# haarcascade_frontalface_alt2.xml is a detector designed for frontal faces
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+
 while(True):
     # read a single frame from the carema
     # ret is a boolean result showing whether the frame is successfully read
     # frame is the frame captured
     ret, frame = cap.read()
     if ret:
+        # covert the color frame into grayscale
+        # The model is based on brightness (grayscale) changes instead of colors
+        # Color only adds unnecessary computation
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # scan the image at different scales to find faces at different distances to the camera
+        # scale factor decides how much the image size is reduced at each scale
+        # minNeighbour decides the number of rectangles must be kept to be recognized as a face
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            print(x, y, w, h)
+            # cv2.rectangle('frame', 'top-left coordinate', 'bottom-right coordinate', 'color of the rectangle', 'line thickness')
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+            cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 5)
+
         #display the frame in a window called frame
         cv2.imshow('frame', frame)
+        cv2.imshow('gray', gray)
     #quit the program when pressing 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
